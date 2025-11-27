117b3b3e687862d4cfe76d3e9f9dc43c1680aa75
susugadx <susugadx777@gmail.com>
2025-11-28 07:03:25 +0900

    chore: test commit for PR

--- src/app/playground/marketing/page.tsx
+++ src/app/playground/marketing/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/marketing/Header"
+import { Hero } from "@/components/marketing/Hero";
 import { HeroTwoColumn } from "@/components/marketing/HeroTwoColumn"
 
 export default function Page() {
@@ -6,6 +7,7 @@ export default function Page() {
     <div>
       <Header />
       <HeroTwoColumn />
+      <Hero />
     </div>
-  );
+    );
 }
