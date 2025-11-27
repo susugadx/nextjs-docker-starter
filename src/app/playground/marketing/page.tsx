7e054a08de0c9700e65393484c9194d7f5a71289
susugadx <susugadx777@gmail.com>
2025-11-28 06:18:05 +0900

    src/app/playground/marketing/page.tsx

---
+++ src/app/playground/marketing/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/marketing/Header"
+import { HeroTwoColumn } from "@/components/marketing/HeroTwoColumn"
+
+export default function Page() {
+  return (
+    <div>
+      <Header />
+      <HeroTwoColumn />
+    </div>
+  );
+}
